832b431144f96693be5f6fde9c37660d05fa3942
Hosein Asadollahi <hosein@gmail.com>
2023-10-28 11:51:46 +0330

    responsive

--- src/Components/button_register/button_register.tsx
+++ src/Components/button_register/button_register.tsx
@@ -5,7 +5,7 @@ import classes from "./button_register.module.css";
 const ButtonRegister: React.FC = (small) => {
   return (
     <div className={classes.button_show_register}>
-      <NavLink to="register" className={classes.button} style={{ maxWidth: `${small ? "177px" : "225px"}` }}>
+      <NavLink to="register" className={classes.button}>
         <p>Register Now</p>
       </NavLink>
     </div>
